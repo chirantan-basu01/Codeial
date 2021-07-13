03bf021540e88ff3d1bf60db65e714460f333914
chirantan-basu01 <basuchirantan001@gmail.com>
2021-07-13 16:12:54 +0530

    creating and altering a cookie

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+
+app.use(cookieParser());
 
 //use express router
 app.use('/', require('./routes'));
